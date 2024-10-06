3c5bf8d0aee4c3276eb216a686600636f1bccc79
yaya mayaya <semenov.aleksandr@phystech.edu>
2024-10-06 18:04:52 +0300

    Исправил задание 1

--- task_1/src/read_cmd.c
+++ task_1/src/read_cmd.c
@@ -10,12 +10,14 @@ int read_cmd(char *string_ptr)
 {
     assert(string_ptr);
 
-    if (!read(STDIN_FILENO, string_ptr, sizeof(string_ptr)))
+    if (!scanf("%[^\n]", string_ptr))
     {
         LOG("couldn't read your input");
         _CLOSE_LOG();
         return INPUT_READ_ERR;
     }
+
+    LOG("string read is: %s", string_ptr);
 
     return 0;
 }

96a7de1a4a8763cb030c7e4bd5207d4cc138a2ea
yaya mayaya <semenov.aleksandr@phystech.edu>
2024-10-22 00:37:04 +0300

    программа для передачи файла через очереди сообщений

---
+++ task_3/msg_queues_src/include/mq_func.h
@@ -0,0 +1,46 @@
+#ifndef MSG_QUEUES
+#define MSG_QUEUES
+
+//#define _GHU_SOURCE
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/msg.h>
+
+#define MSG_SIZE 1020
+
+#define KEY_NAME "./key_to_your_heart"
+
+typedef struct
+{
+    char shift[4];
+    char *msg;
+} msg_t;
+
+struct msgbuf
+{
+    long    mtype;
+    char    msg[MSG_SIZE];
+};
+
+typedef struct
+{
+    const char     *key_name;
+    key_t           key;
+    int             mq_id;
+    struct msgbuf  *msgbuf;
+    size_t          buf_size;
+} mq_st;
+
+int mq_st_create(mq_st *mq, const char *key_name);
+
+void mq_st_destr(mq_st *mq);
+
+int send_file_w_mq(mq_st *mq, const char *file_name);
+
+int rcv_file_w_mq(const char *rcv_file_name, mq_st *mq);
+
+int create_buff(mq_st *mq);
+
+
+#endif

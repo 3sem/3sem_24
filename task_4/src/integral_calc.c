c2ebac0bfb5fbab4a647fe5ee61c55e4ac64c806
yaya mayaya <semenov.aleksandr@phystech.edu>
2024-11-14 02:36:31 +0300

    добавил нити в основную функцию вычисления

--- task_4/src/integral_calc.c
+++ task_4/src/integral_calc.c
@@ -2,76 +2,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+#include <pthread.h>
 #include <math.h>
 #include "integral_calc.h"
+#include "send_results.h"
+#include "thread_funcs.h"
 
-int set_points(point_t *points);
-
-double point_belonging_check(point_t *points, double(alg_func)(double));
-
-
-int set_points(point_t *points)
+int calculate_integral(double(math_func)(double))
 {
-    double step = sqrt(pow(MAX_COORD, 2) / (double)POINTS_NUMBER);
-    LOG("> step is: %lf\n", step);
+    pthread_t           threads[NUMBER_OF_THREADS] = {};
+    integr_func_args    args   [NUMBER_OF_THREADS] = {};
 
-    long long int i = 0;
-    for (double y = 0; (y < MAX_COORD) && (i < POINTS_NUMBER); y += step)
-    {
-        for (double x = 0; (x < MAX_COORD) && (i < POINTS_NUMBER); x += step)
-        {
-            points[i].x = x;
-            points[i].y = y;
+    long int    *results       = NULL;
+    int         shm_fd         = 0;
 
-            i++;  
-        }
-    }
-
-    return 0;
-    
-}
-
-double point_belonging_check(point_t *points, double(alg_func)(double))
-{
-    assert(points);
-
-    double result = 0;
-    long long int points_num = 0;
-    for (int i = 0; i < POINTS_NUMBER; i++)
-    {
-        result = alg_func(points[i].x);
-        if (result > MAX_COORD)
-        {
-            printf("Warning");
-        }
-        
-        points_num += (result < points[i].y);
-    }
-
-    printf("points: %lld\n", points_num);
-
-    return ((double)points_num / POINTS_NUMBER);
-}
-
-int calculate_integral(double(alg_func)(double))
-{
     point_t *points = (point_t *)calloc(POINTS_NUMBER, sizeof(point_t));
-    RETURN_ERROR_ON_TRUE(!points, 11, 
-        printf("> points array memory allocation error\n"););
-    
-    char *belongs_bool = (char *)calloc(POINTS_NUMBER, sizeof(char));
-    RETURN_ERROR_ON_TRUE(!belongs_bool, 12,
-        perror("> memory allocation error\n"););
+    RETURN_ERROR_ON_TRUE(!points, -1, printf("> memory allocation for points error\n"););
 
-    set_points(points);
-    double percent = point_belonging_check(points, alg_func);
+    shm_fd = result_shm_arr_create(&results, NUMBER_OF_THREADS * sizeof(long int));
+    RETURN_ERROR_ON_TRUE(shm_fd == -1, -1);
 
-    double integral = MAX_COORD * MAX_COORD * percent;
-    printf("> result is: %lf\n", integral);
+    for (int i = 0; i < NUMBER_OF_THREADS; i++)
+    {
+        args[i].math_func           = math_func;
+        args[i].points_arr          = points + (POINTS_PER_THREAD * i);
+        args[i].res_ptr             = results + i;
+        args[i].thread_number       = i;
+    }
 
+    int err_num = 0;
+    for (int i = 0; i < NUMBER_OF_THREADS; i++)
+    {
+        err_num = pthread_create(threads + i, 0, thread_integr_func, args + i);
+        RETURN_ERROR_ON_TRUE(err_num, err_num,
+            printf("thread creation error\n"););
+    }
+
+    for (int i = 0; i < NUMBER_OF_THREADS; i++)
+    {
+        RETURN_ERROR_ON_TRUE(pthread_join(threads[i], NULL), -1);
+    }
+
+    shm_close(results, NUMBER_OF_THREADS * sizeof(long int), shm_fd);
     free(points);
-    free(belongs_bool);
 
     return 0;
-    
 }

5c4c2581d700c06e5ad6cf797e2ea1405805b9db
yaya mayaya <semenov.aleksandr@phystech.edu>
2024-11-14 02:37:04 +0300

    некоторые математические функции

---
+++ task_4/src/math_funcs.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include "math_funcs.h"
+
+double square(double x)
+{
+    return x * x;
+}
+
+double root(double x)
+{
+    return sqrt(x);
+}
+
+double linear(double x)
+{
+    return 4 * x + 11;
+}
+
+double mnogochlen(double x)
+{
+    return 4 * x * x - 3 * x + 10;
+}
+
+double expon(double x)
+{
+    return exp2(x);
+}

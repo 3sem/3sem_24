aa8c7031c4d05f473990718e5dfcbf8b339a843c
yayamayaya <semenov.aleksandr@phystech.edu>
2024-11-19 18:32:41 +0300

    добавил 3-ий аргумент

--- run_task_6.sh
+++ run_task_6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #valgrind --leak-check=full --show-leak-kinds=all 
-build/task_6/processmon/processmon $1 $2
+build/task_6/processmon/processmon $1 $2 $3
 echo "> program finished with exit code $?"

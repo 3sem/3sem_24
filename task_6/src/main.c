621d5844d9124d4c7767dad46c1cddaf4a745d65
yayamayaya <semenov.aleksandr@phystech.edu>
2024-11-19 18:33:55 +0300

    добавил новое условие на количество аргументов командной строки

--- task_6/src/main.c
+++ task_6/src/main.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char const *argv[])
 {
-    RETURN_ON_TRUE(argc != 2, ARGC_NOT_TWO, printf("procesmon: Not enough arguments. Specify process PID. For more information use -h\n"););
+    RETURN_ON_TRUE(argc < 2, ARGC_NOT_TWO, printf("processmon: Not enough arguments. Specify process PID. For more information use -h\n"););
+    RETURN_ON_TRUE(argc > 3, ARGC_TOO_MANY, printf("processmon: Too many arguments in a call. For more information use -h\n"););
 
     RETURN_ON_TRUE(execute_option(argv[1]) == ERR_OPT, ERR_OPT);
-
 
     return 0;
 }

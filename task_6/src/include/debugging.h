1950dc49626c0737aea4469ec19dfe5bef528638
yayamayaya <semenov.aleksandr@phystech.edu>
2024-11-22 23:38:00 +0300

    временно отключил логи

--- task_6/src/include/debugging.h
+++ task_6/src/include/debugging.h
@@ -1,7 +1,7 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#define LOG(...) fprintf(stderr, __VA_ARGS__)
+#define LOG(...) //fprintf(stderr, __VA_ARGS__)
 
 #define RETURN_ON_TRUE(statement, ret_val, ...)             \
     do                                                      \

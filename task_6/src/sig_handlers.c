9f220d838ead9ff9b652f89803071e596d766f9d
yayamayaya <semenov.aleksandr@phystech.edu>
2024-11-22 23:37:47 +0300

    добавил новые обработчики сигнала

--- task_6/src/sig_handlers.c
+++ task_6/src/sig_handlers.c
@@ -1,19 +1,71 @@
 #include <stdio.h>
 #include <signal.h>
+#include <unistd.h>
 #include "debugging.h"
+#include "config_changing_funcs.h"
 #include "sig_handlers.h"
 
-static volatile sig_atomic_t parent_sigint = 0;
+/*
+В интерактивном режиме имеем 2 процесса: интерфейс и техническая часть
+Соответственно тут приведены обработчики для соответствущих процессов
+*/
 
-void functional_process_sigint(int signum)
+static volatile sig_atomic_t technical_sigint_status   = 0;
+static volatile sig_atomic_t interface_sigint_status   = 0;
+static volatile sig_atomic_t interface_sigchld_status  = 0;
+
+int signal_handler_set(handler_t (*handler)(int), const int signal_num)
+{
+    struct sigaction new_action = {};
+    new_action.sa_handler = handler;
+    new_action.sa_flags = (int)SA_RESETHAND;
+
+    RETURN_ON_TRUE(sigaction(signal_num, &new_action, NULL) == -1, -1, perror("signal handler set error\n"););
+
+    return 0;
+}
+
+handler_t technical_sigint(int signum)
 {
     LOG("> child process finished\n");
-    parent_sigint = signum;
+    technical_sigint_status = signum;
 
     return;
 }
 
-int check_if_signaled()
+[[noreturn]] handler_t interface_sigint(int signum)
 {
-    return parent_sigint;
+    LOG("> interface was interupted");
+    technical_sigint(SIGINT);
+
+    interface_sigint_status = signum;
+
+    destruct_cfg_fifo();
+
+    _exit(SIGINT);
 }
+
+handler_t interface_sigchld(int signum)
+{
+    LOG("> child process returned error\n");
+    interface_sigchld_status = signum;
+
+    return;
+}
+
+int check_interface_signals()
+{
+    RETURN_ON_TRUE(interface_sigchld_status, interface_sigchld_status);
+    RETURN_ON_TRUE(interface_sigint_status, interface_sigint_status);
+
+    return 0;
+}
+
+int check_technical_signals()
+{
+    RETURN_ON_TRUE(technical_sigint_status, technical_sigint_status);
+
+    return 0;
+}
+
+//Сделать обработчик для интерфейса (в случае ошибки)

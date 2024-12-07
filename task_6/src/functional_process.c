3bcd0f80ccc8c27f62bccf55536f940f18380542
yayamayaya <semenov.aleksandr@phystech.edu>
2024-12-07 19:12:00 +0300

    инициализация структуры tmp_st, переименовал название файла

--- task_6/src/functional_process.c
+++ task_6/src/functional_process.c
@@ -3,15 +3,13 @@
 #include <signal.h>
 #include <limits.h>
 #include <unistd.h>
-#include <sys/inotify.h>
 #include "debugging.h"
 #include "sig_handlers.h"
 #include "parameters_changing.h"
 #include "functional_part.h"
 #include "parse_standard_config.h"
 #include "find_file_diff.h"
-
-//ability to change standart config
+#include "tmp_dir_st.h"
 
 int functional_process(const pid_t pid_to_monitor, const int ipc_fd)
 {
@@ -20,19 +18,15 @@ int functional_process(const pid_t pid_to_monitor, const int ipc_fd)
     int ret_val                         = 0;
     config_st config                    = {};
     RETURN_ON_TRUE(init_cfg_struct(&config) == MEM_ALC_ERR, MEM_ALC_ERR,
-        printf("Processmon: couldn't allocate memory for temp directory path in config\n"););
+        printf(MEM_ALC_ERR_MSG););
 
     config.monitoring_pid               = pid_to_monitor;
 
     RETURN_ON_TRUE(load_standard_config(&config) == -1, -1, destr_cfg_struct(&config););
 
-    char path[PATH_MAX]                 = {0};
-    snprintf(path, PATH_MAX * sizeof(char), "./%d.txt", pid_to_monitor);
-
-    LOG("the result of combination is: %s\n", path);
-
-    ret_val = create_tmp_dir(config.tmp_folder_path);
-    RETURN_ON_TRUE(ret_val == -1, -1, 
+    tmp_st *temp_dir = NULL;
+    ret_val = tmp_dir_init(&temp_dir, config.tmp_folder_path);
+    RETURN_ON_TRUE(ret_val, ret_val, 
         perror("couldn't create temp directory");
         destr_cfg_struct(&config););
 
@@ -43,20 +37,17 @@ int functional_process(const pid_t pid_to_monitor, const int ipc_fd)
 
         sleep(config.period);
 
-        ret_val = update_config(&config, ipc_fd);
+        ret_val = update_config(&config, temp_dir, ipc_fd);
         if (ret_val && ret_val != TMP_CNG_ERR)
             break;
 
-        ret_val = file_diff(path, config.tmp_folder_path);
+        ret_val = file_diff(config.monitoring_pid, temp_dir);
         if (ret_val && ret_val != TMP_CNG_WAIT)
             break;
-        
-        //RETURN_ON_TRUE(write(config.diff_file_fd, ">\n", 2 * sizeof(char)) == -1, -1, perror("couldn't write data"););
     }
-    if (config.tmp_delete_bool) 
-        clear_tmp(config.tmp_folder_path);
 
     close(ipc_fd);
+    temp_dir->methods.dir_desctr(temp_dir, config.tmp_delete_bool);
     destr_cfg_struct(&config);
     
     return ret_val;
